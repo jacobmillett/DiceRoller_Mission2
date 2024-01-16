f79d2f5a0e0c26384e962ac0d941657e111f98c8
jacob <jacob@JACOB74FC>
2024-01-16 16:25:24 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using DiceRoller;
+using System;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.Write("Welcome to the Dice Rolling Game!\n\nHow many dice rolls would you like to simulate?");
+
+        string promptInput = Console.ReadLine();
+
+        if (int.TryParse(promptInput, out int rollCount))
+        {
+            Rolls diceRoller = new Rolls(rollCount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer in number form.");
+        }
+
+    }
+}
---
+++ Rolls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class Rolls
+    {
+        public int diceRolls;
+        public int[] counters = new int[12];
+        public string displayMessage;
+
+
+        public Rolls(int diceRolls)
+        {
+            this.diceRolls = diceRolls;
+            this.displayMessage = $"DICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.\r\nTotal number of rolls = {diceRolls}.\n\n";
+
+            Random random = new Random();
+            for (int iCount=0; iCount<diceRolls; iCount++)
+            {
+                int firstDie = random.Next(1, 7);
+                int secondDie = random.Next(1, 7);
+                int dieSum = firstDie + secondDie;
+                counters[dieSum-1]++;
+            }
+
+            for (int iCount = 0; iCount <= counters.Length - 2; iCount++)
+            {
+                int percentage = (counters[iCount + 1] * 100) / diceRolls;
+                displayMessage += $"{iCount + 2}: {new string('*', percentage)}\n";
+            }
+
+            Console.WriteLine(displayMessage);
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+            
+        }
+        
+            
+
+        
+
+
+
+    }
+}
